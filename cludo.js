136f23da88b15bd68f3d461b758365b903d8c5f1
Louise <lou_stewart@live.co.uk>
2018-12-18 08:56:09 +0000

    initial commit

---
+++ cludo.js
@@ -0,0 +1,244 @@
+// Scope Homework: Who Dunnit
+// Learning Objectives
+// Understand function scope
+// Know the difference in between the let and const keywords
+// Brief
+// Using your knowledge about scope and variable declarations in JavaScript, look at the following code snippets and predict what the output or error will be and why. Copy the following episodes into a JavaScript file and add comments under each one detailing the reason for your predicted output.
+
+MVP
+Episode 1
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Miss Scarlet
+// This the murderer declared in the constatant 'sceanrio' which is called in the declareMurderer function
+
+
+
+Episode 2
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Professor Plum
+// Verdict is equal to declareMurderer; the declareMurderer function does not call changeMurderer
+// declareMurderer reverts to the original murderer
+
+
+
+Episode 3
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// First Verdict: Mrs Peacock
+// First verdict looks for the declareMurderer function which details Mrs. Peacock
+
+// Second Verdict: Professor Plum
+// Second verdict calls the murderer (does not look for declareMurderer) so returns the original murderer
+
+
+
+Episode 4
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+// Miss Scarlet, Professor Plum, Colonel Mustard
+// (suspects) returns the suspects detailed in the declareAllSuspects function in which suspect three is changed to Col Mustard
+
+// Suspect three is Mrs Peacock
+// In the second log, this does not call suspects or declareAllSuspects so returns the original suspectThree
+
+
+
+Episode 5
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
+
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
+
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
+
+// The weapon is the Revolver
+// The log prints the (verdict) which is equal to declareWeapon which in turn returns scenario.weapon
+// sceanrio.weapon calls newWeapon which is the result of the changeWeapon function
+// change waepon is declared as Revolver
+
+
+
+Episode 6
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+
+// The murderer is Colonel Mustard
+// log requests (verdict) which is defined as declareMurderer which in turn calls murderer
+// murderer is defined as Colonel Mustard
+
+// However, I'm unsure if the changeMurderer in the third last line has an impact!
+// If so, the murderer is Mr Green
+
+
+
+
+Episode 7
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// The murderer is Professor Plum
+// log calls (verdict) which in turn calls declareMurderer
+// declareMurderer calls the murderer which is Professor Plum
+
+// However, I'm unsure if the changeMurderer in the third last line has an impact!
+// If so, the murderer is Mr Green
+
+
+
+Episode 8
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory',
+  weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
+
+// The weapon is Lead Pipe
+// verdict = declareWeapon which is Lead Pipe
+
+
+
+Episode 9
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+
+// The murderer is Mrs. Peacock
+// The if statement states that if murderer strictly equals Professor Plum then murderer should then equal Mrs. Peacock
